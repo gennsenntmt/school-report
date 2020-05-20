256a2c7cfcde575591d673bc4755b00fdf0e6fec
gennsenntmt <56209451+gennsenntmt@users.noreply.github.com>
2020-05-20 15:21:02 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'reports#index'
+  get 'users/:id' => 'users#show'
+  get 'search', to: 'reports#search'
+  
+  
+  resources :users do 
+    resources :reports, only: [:new,:create] 
+  end
+
+  resources :students , only: [:show,:new,:create,:edit,:update,:destroy]
+  resources :reports
+
 end
